d6a40cc8e278c5ec004bcea3c451046a64cb6426
lamhongphong512 <117129211+lamhongphong512@users.noreply.github.com>
2023-12-23 16:13:19 +0700

    Add files via upload

--- present.js
+++ present.js
@@ -7,12 +7,14 @@ var nametag = document.getElementById("nametag");
 var present = document.getElementById("present");
 var presentImage = document.getElementById("present-image");
 
+
 function init() {
   var graphElem = document.querySelector('.present-box > .side.top .to');
   graphElem.setAttribute('data-before', eventName);
   document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
   document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
   document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+
 
   var _giftLink, 
       _giftImg;
